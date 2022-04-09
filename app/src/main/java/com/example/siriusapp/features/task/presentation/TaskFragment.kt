a4633304142c64f5f8cc7843df3d176af1000edd
Grzegorz Brzęczyszczykiewicz <general.m.r.pochka@gmail.com>
2022-04-09 10:58:47 +0300

    Change focus in edittexts

--- app/src/main/java/com/example/siriusapp/features/task/presentation/TaskFragment.kt
+++ app/src/main/java/com/example/siriusapp/features/task/presentation/TaskFragment.kt
@@ -5,10 +5,13 @@ import android.content.Context.BIND_AUTO_CREATE
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
-import android.os.Handler
 import android.os.IBinder
-import android.provider.Contacts
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
@@ -16,6 +19,7 @@ import com.example.siriusapp.R
 import com.example.siriusapp.databinding.FragmentTaskBinding
 import com.example.siriusapp.features.task.presentation.service.SoundService
 import kotlinx.coroutines.*
+import java.util.ArrayList
 
 
 class TaskFragment : Fragment(R.layout.fragment_task) {
@@ -24,6 +28,9 @@ class TaskFragment : Fragment(R.layout.fragment_task) {
     private var number1 = 0
     private var number2 = 0
     private var number3 = 0
+    private var arrayEt = ArrayList<EditText>(3)
+    private var current: Int = 0
+    private var isCheck: Boolean = false
 
     private val connection = object : ServiceConnection {
         override fun onServiceConnected(
@@ -59,6 +66,7 @@ class TaskFragment : Fragment(R.layout.fragment_task) {
             btnContinue.setOnClickListener {
                 endTask()
             }
+            changeEditTexts()
         }
     }
 
@@ -74,26 +82,32 @@ class TaskFragment : Fragment(R.layout.fragment_task) {
                 delay(1000L)
                 tvInfo.text = getString(R.string.listen_sound)
                 chooseAudios()
-                binder?.play(R.raw.n20)
+                startMedia()
                 btnCheck.visibility = View.VISIBLE
             }
         }
     }
 
     private fun chooseAudios() {
-        number1 = 20
-        number2 = 20
-        number3 = 20
-        /*while (number2 == number1) {
+        number1 = (10..20).random()
         number2 = (10..20).random()
-    }
-    while (number2 == number3 || number1 == number3) {
         number3 = (10..20).random()
-    }*/
+        while (number2 == number1) {
+            number2 = (10..20).random()
+        }
+        while (number2 == number3 || number1 == number3) {
+            number3 = (10..20).random()
+        }
     }
 
-    private fun startMedia(n1: Int, n2: Int, n3: Int) {
-        //TODO
+    private fun startMedia() {
+        lifecycleScope.launch {
+            binder?.play(resources.getIdentifier("n" + 20.toString(), "raw", activity?.packageName))
+            delay(400)
+            binder?.play(R.raw.n17eng)
+            delay(400)
+            binder?.play(R.raw.n14)
+        }
     }
 
     private fun check() {
@@ -126,5 +140,71 @@ class TaskFragment : Fragment(R.layout.fragment_task) {
 
     private fun endTask() {
         view?.findNavController()?.navigate(R.id.action_taskFragment_to_selectTaskFragment)
+    }
+
+    private fun changeEditTexts() {
+        with(binding) {
+            arrayEt.add(edAnswer1)
+            arrayEt.add(edAnswer2)
+            arrayEt.add(edAnswer3)
+            edAnswer1.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                override fun afterTextChanged(s: Editable?) {
+                    if (s?.length == 2 && !isCheck) {
+                        current++
+                        if (current < 4) {
+                            arrayEt[current].requestFocus()
+                        }
+                    }
+                }
+            })
+            edAnswer2.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                override fun afterTextChanged(s: Editable?) {
+                    if (s?.length == 2 && !isCheck) {
+                        current++
+                        if (current < 4) {
+                            arrayEt[current].requestFocus()
+                        }
+                    }
+                }
+            })
+            edAnswer3.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                override fun afterTextChanged(s: Editable?) {
+                    if (s?.length == 2 && !isCheck) {
+                        ViewCompat.getWindowInsetsController(requireView())
+                            ?.hide(WindowInsetsCompat.Type.ime())
+                    }
+                }
+            })
+        }
     }
 }
